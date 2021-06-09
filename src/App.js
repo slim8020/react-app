8568cbd5e88406e2195c1a8048f69586fd78c69e
slim8020 <slim8020@naver.com>
2021-06-09 13:12:21 +0900

    modify README.md

--- src/App.js
+++ src/App.js
@@ -1,26 +1,53 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
 
-function App() {
+
+class Subject extends Component {
+  render(){
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="Subject">
+        <h1>Web</h1>
     </div>
   );
+  }
+}
+
+
+class TOC extends Component {
+  render(){
+    return(
+        <nav>
+          <ul>
+            <li><a href="1.html">HTML</a></li>
+            <li><a href="2.html">CSS</a></li>
+            <li><a href="3.html">Javascript</a></li>
+          </ul>
+        </nav>
+    );
+  }
+}
+class Content extends Component{
+  render(){
+    return (
+      <article>
+          <h2>HTML</h2>
+          HTML is HyperText Markup Language. 
+      </article>
+    );
+  }
+}
+
+
+class App extends Component {
+  render(){
+  return (
+    <div className="App">
+      <Subject/>
+      <TOC/>
+      <Content/>
+    </div>
+  );
+  }
 }
 
 export default App;
